dd7ba73da47d7695f53bdfd935154f1121ae9049
David Montaño <cuatro04sr@gmail.com>
2023-02-15 21:52:46 -0500

    add cars toString and print

--- src/Car.java
+++ src/Car.java
@@ -7,4 +7,18 @@ public class Car {
         this.wheels = wheels;
         this.cylinders = cylinders;
     }
+
+    public boolean isWheels() {
+        return wheels;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    @Override
+    public String toString() {
+        return "Wheels: " + wheels + " cylinders: " + cylinders + "\n";
+    }
 }
+
